87b179e479802ff5df2993c15e8aa2c3a78cefd0
gids-dadi <gideons564@gmail.com>
2022-04-09 21:40:14 +0100

    clear linter erros

--- JS/work.js
+++ JS/work.js
@@ -238,8 +238,7 @@ const displayPopupModal = (i) => {
 // Open
 document
   .querySelectorAll('.works-button')
-  .forEach((row) => row.addEventListener('click', () => displayPopupModal(row.getAttribute('data-index'))),
-  );
+  .forEach((row) => row.addEventListener('click', () => displayPopupModal(row.getAttribute('data-index'))), );
 // close pop
 const closeModal = document.querySelector('#portfolioPopup');
 // eslint-disable-next-line no-unused-vars
