761637f0d8e3ac91bd3bbd368e93e9b385baa990
gids-dadi <gideons564@gmail.com>
2022-04-09 21:28:21 +0100

    clear linter erros

--- JS/work.js
+++ JS/work.js
@@ -238,8 +238,7 @@ const displayPopupModal = (i) => {
 // Open
 document
   .querySelectorAll('.works-button')
-  .forEach((row) =>
-    row.addEventListener('click', () =>
+  .forEach((row) =>row.addEventListener('click', () =>
       displayPopupModal(row.getAttribute('data-index'))
     )
   );
