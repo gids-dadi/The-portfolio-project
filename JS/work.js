f07ab876f7abe153204de2db10eeb0dcda10003c
gids-dadi <gideons564@gmail.com>
2022-04-10 22:34:48 +0100

    Make the form validation display

--- JS/work.js
+++ JS/work.js
@@ -107,6 +107,38 @@ const cardDetails = [
   },
 ];
 
+// Mobile Menu
+const hamburger = document.querySelector('#menu-mobile');
+const menuLinks = document.querySelector('.nav-list');
+const logoClass = document.querySelector('.name_logo');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+  logoClass.classList.toggle('closeLogo');
+});
+
+const portfolio = document.querySelector('.portfolio-link');
+const aboutUs = document.querySelector('.about-link');
+const contactLink = document.querySelector('.contact-link');
+
+const close = () => {
+  hamburger.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+portfolio.addEventListener('click', () => {
+  close();
+});
+
+aboutUs.addEventListener('click', () => {
+  close();
+});
+
+contactLink.addEventListener('click', () => {
+  close();
+});
+
 for (let i = 0; i < 1; i += 1) {
   let techList = '';
   for (let j = 0; j < cardDetails[0].technologies.length; j += 1) {
